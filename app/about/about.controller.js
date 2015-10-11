8ef5781bd6daeb32db5d070706e709a74cd6144d
Brad <brad.anderson58@gmail.com>
2015-10-11 11:16:48 -0400

    fixed broken link to demos (my bad)

--- app/about/about.controller.js
+++ app/about/about.controller.js
@@ -9,7 +9,7 @@ function AboutController(){
     
     var ball_details = "The amazing World of Ballcraft is the first 'game' devloped in OmniEngine.  Play as a ball, roll around a procedurally generated world.  Jump around and enjoy our physics.  Log on and roll with your friends, or just watch the sunset thanks to our day/night time system.";
     var teapot_details = "This glass teapot shows off some of the amazing capabilities of the WebGL graphics library.  Luckily, with OmniEngine, you don't have to be a hardcore graphics programmer to render cool scenes like this";
-    var demos_details = "Check out our old demos page.  There's some cool stuff on here.  Eventually, we will have a new and improved demos page, but for now, mess around on here.";
+    var demos_details = "Check out our demos page.  There's some cool stuff on here.  Eventually, we will have a new and improved demos page, but for now, mess around on here.";
     var oculus_details = "We are currently developing stereoscopic capabilities for support of virtual reality systems such as the Oculus Rift!";
     
     //separate this stuffs to games and demos (we should also probably JSON ize all this info at some point)
@@ -39,7 +39,7 @@ function AboutController(){
                 'details': teapot_details
             },
             {'name':'Demo Page',
-             'address': 'danglingpointers.me/demos/',
+             'address': '/demos/',
              'photo': "assets/OmniVerse.png",
              'details': demos_details
             }
