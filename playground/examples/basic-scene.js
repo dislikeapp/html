5be31694832dbb8bba97f6c592459d139ab53941
TomKrcmar <tomkrcmar@gmail.com>
2015-12-12 16:07:24 -0500

    new stuff

--- playground/examples/basic-scene.js
+++ playground/examples/basic-scene.js
@@ -1,6 +1,6 @@
 var sphere = scene.addObject(new OE.Sphere());
 sphere.setMaterial("DefaultWhite");
 
-var plane = scene.addObject(new OE.TerrainPatch(10,10));
+var plane = scene.addObject(new OE.Plane(10,10));
 plane.setMaterial("DefaultWhite");
 plane.setPosf(0,-1,0);
